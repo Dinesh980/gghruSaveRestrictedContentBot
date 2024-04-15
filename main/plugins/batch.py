66f0d89d109a98aac8d35f2be901cfb39068c81f
Dinesh980 <139450574+Dinesh980@users.noreply.github.com>
2024-04-15 20:32:38 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -66,8 +66,8 @@ async def _batch(event):
                 return conv.cancel()
             try:
                 value = int(_range.text)
-                if value > 100:
-                    await conv.send_message("You can only get upto 100 files in a single batch.")
+                if value > 100000:
+                    await conv.send_message("You can only get upto 100000 files in a single batch.")
                     return conv.cancel()
             except ValueError:
                 await conv.send_message("Range must be an integer!")
@@ -79,13 +79,17 @@ async def _batch(event):
 
 async def run_batch(userbot, client, sender, link, _range):
     for i in range(_range):
-        timer = 60
+        timer = 1
         if i < 25:
+            timer = 4
+        if i < 250 and i > 25:
             timer = 5
-        if i < 50 and i > 25:
-            timer = 10
-        if i < 100 and i > 50:
-            timer = 15
+        if i < 2500 and i > 250:
+            timer = 6
+        if i < 25000 and i > 2500:
+            timer = 7
+        if i < 100000 and i > 25000:
+            timer = 8
         if not 't.me/c/' in link:
             if i < 25:
                 timer = 2
@@ -102,7 +106,7 @@ async def run_batch(userbot, client, sender, link, _range):
         try:
             await get_bulk_msg(userbot, client, sender, link, i) 
         except FloodWait as fw:
-            if int(fw.x) > 299:
+            if int(fw.x) > 2999:
                 await client.send_message(sender, "Cancelling batch since you have floodwait more than 5 minutes.")
                 break
             await asyncio.sleep(fw.x + 5)
